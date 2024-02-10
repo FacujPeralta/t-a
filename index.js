35c8e5d90b5368a1446af34831367a52e76b2842
FacundoPeralta <facuperalta_@outlook.es>
2024-02-10 10:57:53 -0300

    .

--- index.js
+++
@@ -1,9 +0,0 @@
-class productos{
-    constructor(id,nombre,precio, img){
-        this.id = id; 
-        this.nombre = nombre;
-        this.precio = precio;
-        this.img = img;
-        this.cantidad = 1;//Cada vez que se sume, va a ser 1 a la vez
-    }
-}
