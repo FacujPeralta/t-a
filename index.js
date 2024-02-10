0ddddedafd614bba5c0487538678cd9ce849f4eb
FacundoPeralta <facuperalta_@outlook.es>
2024-02-10 10:55:39 -0300

    Finalización de la parte front-end para la empresa TODO ABERTURAS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+class productos{
+    constructor(id,nombre,precio, img){
+        this.id = id; 
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img;
+        this.cantidad = 1;//Cada vez que se sume, va a ser 1 a la vez
+    }
+}
